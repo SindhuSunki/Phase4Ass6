ab82025ad9237ee1a557c3c95fd5416dc5f34e52
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-09-19 16:21:48 +0530

    first commit

---
+++ Assignment6/Models/BookstoreDbContext.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment6.Models;
+
+public partial class BookstoreDbContext : DbContext
+{
+    public BookstoreDbContext()
+    {
+    }
+
+    public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    public virtual DbSet<BookCategory> BookCategories { get; set; }
+
+    public virtual DbSet<Publisher> Publishers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=tcp:sindhu789.database.windows.net,1433;Initial Catalog=BookstoreDb;User Id=Sindhus;Password=Sindhu@789;Encrypt=True;TrustServerCertificate=True;Connection Timeout=60;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.AuthorId).HasName("PK__Author__70DAFC34C830892C");
+
+            entity.ToTable("Author");
+
+            entity.Property(e => e.AuthorId).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.BookId).HasName("PK__Book__3DE0C207C622AA57");
+
+            entity.ToTable("Book");
+
+            entity.Property(e => e.BookId).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(255);
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Books)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__Book__CategoryId__628FA481");
+
+            entity.HasOne(d => d.Publisher).WithMany(p => p.Books)
+                .HasForeignKey(d => d.PublisherId)
+                .HasConstraintName("FK__Book__PublisherI__6383C8BA");
+
+            entity.HasMany(d => d.Authors).WithMany(p => p.Books)
+                .UsingEntity<Dictionary<string, object>>(
+                    "BookAuthor",
+                    r => r.HasOne<Author>().WithMany()
+                        .HasForeignKey("AuthorId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__BookAutho__Autho__6754599E"),
+                    l => l.HasOne<Book>().WithMany()
+                        .HasForeignKey("BookId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__BookAutho__BookI__66603565"),
+                    j =>
+                    {
+                        j.HasKey("BookId", "AuthorId").HasName("PK__BookAuth__6AED6DC471795E0F");
+                        j.ToTable("BookAuthor");
+                    });
+        });
+
+        modelBuilder.Entity<BookCategory>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__BookCate__19093A0B349EC7DE");
+
+            entity.ToTable("BookCategory");
+
+            entity.Property(e => e.CategoryId).ValueGeneratedNever();
+            entity.Property(e => e.CategoryName).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<Publisher>(entity =>
+        {
+            entity.HasKey(e => e.PublisherId).HasName("PK__Publishe__4C657FAB4F2448FE");
+
+            entity.ToTable("Publisher");
+
+            entity.Property(e => e.PublisherId).ValueGeneratedNever();
+            entity.Property(e => e.Address).HasMaxLength(255);
+            entity.Property(e => e.PublisherName).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
